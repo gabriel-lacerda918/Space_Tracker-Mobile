322d50d1211fd15e66e6d76ebf31a4ea04c25c19
Gabriel Lacerda <161903643+gabriel-lacerda918@users.noreply.github.com>
2026-06-08 23:43:59 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,7 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSYuBDRRbm_IxWMT0FY4FCjQBX7wqW850",
-  authDomain: "fiap-mobile-b1d17.firebaseapp.com",
-  databaseURL: "https://fiap-mobile-b1d17-default-rtdb.firebaseio.com/",
-  projectId: "fiap-mobile-b1d17",
-  storageBucket: "fiap-mobile-b1d17.firebasestorage.app",
-  messagingSenderId: "48135676598",
-  appId: "1:48135676598:web:ae2d5f1f979c3017a7bc17"
+ 
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
